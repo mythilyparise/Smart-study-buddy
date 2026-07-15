e15c7793d86fb815fea2b48794c410c3cbd218ac
{mythily} <mythilyparise@gmail.com>
2026-07-15 11:26:47 +0530

    Update Gemini model

--- utils/llm.py
+++ utils/llm.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 def get_llm():
     return ChatGoogleGenerativeAI(
-        model="gemini-2.5-flash",
+        model="gemini-3-flash-preview",
         google_api_key=os.getenv("GOOGLE_API_KEY"),
         temperature=0.3
     )
